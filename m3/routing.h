9916a9f5dd77301af526ebf4894448e0b5aaa5e6
Himangshu Saikia <arctic.quasar@gmail.com>
2011-09-07 09:38:31 +0200

    milestone3 some comments added

--- m3/routing.h
+++ m3/routing.h
@@ -1,3 +1,9 @@
+/*
+* Code which setups the routing table for every node and shows the 
+* optimum path to a given destination considering the current network
+* congestion
+*/
+
 #include "definitions.h"
 
 typedef struct {
