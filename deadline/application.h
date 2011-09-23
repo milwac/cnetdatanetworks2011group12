8790740ff428ee4c7a9cdf1eb8dd430caca17aaf
Himangshu Saikia <arctic.quasar@gmail.com>
2011-09-23 22:53:35 +0200

    routing queue add error

---
+++ deadline/application.h
@@ -0,0 +1,5 @@
+#include "definitions.h"
+#define MAX_MSG_QUEUE_SIZE 30
+#define MESSAGE_HEADER_SIZE (sizeof(CnetAddr))
+
+QUEUE msg_queue;
---
+++ deadline/network_layer.h
@@ -0,0 +1,28 @@
+#include "routing.h"
+#define MAX_NETWORK_QUEUE_SIZE 200
+
+/*
+* Code which setups the routing table for every node and shows the
+* optimum path to a given destination considering the current network
+* congestion
+*/
+
+//contains all datagrams -- both from self and others
+QUEUE network_queue;
+
+//contains all datagrams received for the self node
+QUEUE receiver_queue;
+
+typedef struct {
+	char incomplete_data[MAX_MESSAGE_SIZE];
+	int current_message;
+	int current_offset_needed;
+	HASHTABLE ooo_data;		
+	int next_msg_to_generate;
+		
+} NODE_BUFFER;
+
+NODE_BUFFER buff[MAX_NODES];
+
+//------------------ Funtion definitions --------------------
+
